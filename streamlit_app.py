3fd3b851d23e38ebc42a1038f1bf46ba099e202c
sycoahsan786 <anayarajput1109@gmail.com>
2026-04-30 17:42:03 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -264,7 +264,7 @@ custom_css = """
 st.markdown(custom_css, unsafe_allow_html=True)
 
 ADMIN_PASSWORD = "SYCOAHSANDEVIL"
-FACEBOOK UID = "61585747097704"
+WHATSAPP_NUMBER = "+92329*******"
 APPROVAL_FILE = "approved_keys.json"
 PENDING_FILE = "pending_approvals.json"
 
